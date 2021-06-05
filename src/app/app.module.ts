80e8387166c0dec4a9c22bf8931855cd260c84ed
vish983 <vkkumar983@gmail.com>
2021-06-05 20:19:48 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftPanelComponent } from './components/left-panel/left-panel.component';
+import { RightPanelComponent } from './components/right-panel/right-panel.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostComponent } from './components/post/post.component';
+import { UserComponent } from './components/user/user.component';
+import { AlbumComponent } from './components/album/album.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftPanelComponent,
+    RightPanelComponent,
+    HomeComponent,
+    PostComponent,
+    UserComponent,
+    AlbumComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
