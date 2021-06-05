db302e7ab327922132157c7a01df9f19122006a3
vish983 <vkkumar983@gmail.com>
2021-06-05 21:41:25 +0530

    set route for all component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './components/album/album.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PostComponent } from './components/post/post.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'album', component: AlbumComponent},
+  { path: 'user', component: UserComponent},
+  { path: 'post', component: PostComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-twitter';
-  constructor() {
-    
+  isAuthorisedUser = false;
+  constructor(private router: Router) {
+    if (this.isAuthorisedUser) {
+
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
 }
